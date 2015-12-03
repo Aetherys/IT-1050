06f187633ffd0c27066d319cfe350b677183f353
Donnie Santos <DonaldJSantos@Gmail.com>
2015-12-02 20:42:42 -0500

    Some More.

---
+++ Assignment2_Donnie_MoreDifferentS/Assignment2_Donnie/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace Assignment2_Donnie
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public Person Spouse { get; set; }
+
+        public void ReadAndSetFirstName() { FirstName = MyConsole.ReadString("First Name"); }
+        public void ReadAndSetLasttName() { LastName = MyConsole.ReadString("Last Name"); }
+        public void ReadAndSetAge() { Age = MyConsole.ReadInt("Age"); }
+        public bool ReadIsMarried() { return MyConsole.ReadBool("Married?"); }
+    }
+}
---
+++ Assignment2_Donnie_MoreDifferentS/Assignment2_Donnie/PersonFactory.cs
@@ -0,0 +1,24 @@
+﻿namespace Assignment2_Donnie
+{
+    public static class PersonFactory
+    {
+        public static Person Create()
+        {
+            Person person = new Person();
+            person.ReadAndSetFirstName();
+            person.ReadAndSetLasttName();
+            person.ReadAndSetAge();
+
+            if (person.ReadIsMarried())
+            {
+                person.Spouse = new Person();
+                person.Spouse.ReadAndSetFirstName();
+                person.Spouse.LastName = person.FirstName;
+                person.Spouse.ReadAndSetAge();
+                person.Spouse.Spouse = person;
+            }
+
+            return person;
+        }
+    }
+}
