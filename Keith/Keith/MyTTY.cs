64d3e3b52fa18c6cca65b0b9e9e12629d041a04d
Aetherys <michaelvmurray@gmail.com>
2015-12-04 20:10:10 -0500

    Keith
    Start of a program to do what Bobbi Jo's son Keith used to ask me all
    the time...
    
    Can I play Diablo?

---
+++ Keith/Keith/MyTTY.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Keith
+{
+    public static class MyTTY
+    {
+        public static void Resize()
+        {
+            Console.SetWindowSize(Console.WindowWidth * 2, Console.WindowHeight * 2);
+        }
+
+
+        public static void Clear()
+        {
+            System.Console.Clear(); 
+        }
+
+
+        public static void Show(string text)
+        {
+            System.Console.Write(text);
+        }
+        
+
+        public static void Continue()
+        {
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+
+        public static string ReadString(string prompt)
+        {
+            Console.Write(prompt.PadLeft(3) + " ");
+            return Console.ReadLine();
+        }
+
+        
+        public static int ReadInt(string prompt)
+        {
+            string input = "";
+            while (!tryParseInt(input)) input = ReadString(prompt);
+            return int.Parse(input);
+        }
+
+        
+        public static bool ReadBool(string prompt)
+        {
+            string input = "";
+            while (!tryParseBool(input)) input = ReadString(prompt);
+            return input.ToLower().StartsWith("y");
+        }
+
+        
+        private static bool tryParseInt(string text)
+        {
+            int result;
+            return int.TryParse(text, out result);
+        }
+
+        
+        private static bool tryParseBool(string text)
+        {
+            if (string.IsNullOrEmpty(text)) return false;
+            if (text.ToLower().StartsWith("y")) return true;
+            if (text.ToLower().StartsWith("n")) return true;
+            return true;
+        }
+    }
+}
---
+++ Keith/Keith/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Keith
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool started = false;
+            int pid = 0;
+            var process = new Process();
+            string filename = "";
+            
+
+            filename = MyTTY.ReadString("Enter the name of the program you wish to run: ");
+            
+
+            ////////////////////////////////////////////////////////////////////////////////
+            /////////////////////////////   START THE PROGRAM   ////////////////////////////
+            ////////////////////////////////////////////////////////////////////////////////
+
+            if (!MyTTY.ReadBool("Are you sure you want to run " + filename + " ?  Y/N "))
+            {
+                MyTTY.Show("Launch Scrub.");
+                MyTTY.Continue();
+            }
+
+            else
+            {
+                process.StartInfo.FileName = filename;
+
+                started = process.Start();
+
+                try
+                {
+                    pid = process.Id;
+                    Console.WriteLine("Ok, " + filename + " started with PID: " + pid);
+                }
+                catch (InvalidOperationException)
+                {
+                    started = false;
+                }
+                catch (Exception e)
+                {
+                    started = false;
+                    Console.Write(e);
+                }
+            }
+
+            
+            ////////////////////////////////////////////////////////////////////////////////
+            //////////////////////////////   STOP THE PROGRAM   ////////////////////////////
+            ////////////////////////////////////////////////////////////////////////////////
+
+
+            if (MyTTY.ReadBool("Ready to kill it now? "))  // Let me look at Task Manager...
+            {
+                MyTTY.Clear();
+            }
+
+            Process[] processByName = Process.GetProcessesByName(filename);
+            int i = processByName.Length;
+
+            MyTTY.Show("There are " + i + " processes with the name " + filename + ".\n");
+
+            while (i > 0)
+            {
+                MyTTY.Show(processByName[i - 1].Id.ToString());
+                i -= 1;
+            }
+
+            Process target;
+            i = processByName.Length;
+
+            while (i > 0)
+            {
+                string id = MyTTY.ReadString("Enter the PID to kill: ");
+
+                if (id == "")
+                    break;
+
+                try
+                {
+                    target = Process.GetProcessById(Int32.Parse(id));
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine("Incorrect entry.");
+                    Console.Write(e);
+                    continue;
+                }
+
+                if (target.ProcessName == filename)
+                {
+                    target.Kill();
+                    target.WaitForExit();
+                }
+
+                i -= 1;
+            }
+                
+                   
+        }
+    }
+}
